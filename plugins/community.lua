0a2513f8f3cd87a111dd760770ec2f5149dae42e
lsy <stillfox.lee@gmail.com>
2023-07-26 09:28:22 +0800

    change color scheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,12 +4,13 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.search.nvim-hlslens" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  -- { import = "astrocommunity.indent.indent-blankline-nvim" },
 
   -- Languages
   { import = "astrocommunity.pack.lua" },
