1370874a22737ba6a5c8e5b58a0ba89e49c107b3
lsy <stillfox.lee@gmail.com>
2023-07-26 09:27:49 +0800

    add todo-comment

--- plugins/user.lua
+++ plugins/user.lua
@@ -13,4 +13,10 @@ return {
     "danilamihailov/beacon.nvim",
     event = "BufRead",
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("todo-comments").setup {} end,
+    event = "User AstroFile",
+  },
 }
