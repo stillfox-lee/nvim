d2a5a640271defeea881c800b9c198585eca3b04
lsy <stillfox.lee@gmail.com>
2023-07-28 10:18:24 +0800

    feat: change color scheme

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ return {
 
   -- Set colorscheme to use
   -- colorscheme = "catppuccin",
-  colorscheme = "onedark",
+  colorscheme = "onedark_dark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
---
+++ plugins/onedark.lua
@@ -0,0 +1,16 @@
+return {
+  "olimorris/onedarkpro.nvim",
+  opts = {
+    options = {
+      highlight_inactive_windows = true,
+    },
+    highlights = {
+      ErrorMsg = { italic = true, bold = true },
+    },
+    styles = {
+      comments = "italic",
+      functions = "bold",
+      methods = "bold",
+    },
+  },
+}
